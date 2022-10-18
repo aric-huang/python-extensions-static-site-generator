411d8af3493dd20670fa6e5d283768472aff0403
aric-huang <arichuang1997@gmail.com>
2022-10-18 17:28:51 -0400

    file changes

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -10,8 +10,9 @@ def load_module(directory, name):
     sys.path.pop(0)
 
 def load_directory(directory):
-    for path in directory:
+    for path in directory.rbglob("*.py"):
         load_module(directory.as_posix(), path.stem)
 
 def load_bundled():
-    directory = Path(__file__).parent/"extensions"
+    directory = Path(__file__).parent / "extensions"
+    load_directory(directory)
